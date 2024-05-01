0149f7c991ee578b4bc2c87013936172c233d254
dazizo25 <68240450+dazizo25@users.noreply.github.com>
2024-05-01 03:20:48 +0100

    Update api.php
    added the contact data in to the API

--- api.php
+++ api.php
@@ -38,6 +38,37 @@ if(isset($_GET['apicall'])){
 	    echo "<p>An error has occured.<br/>The item was not added.</p>";
       }
       $conn->close();
-  }
+
+  case 'insert_contact':
+    $companyName = $_POST['companyName'];
+    $contactName = $_POST['contactName'];
+    $title = $_POST['title'];
+    $role = $_POST['role'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $extensionNumber = $_POST['extensionNumber'];
+    $mobileNumber = $_POST['mobileNumber'];
+    $emailAddress = $_POST['emailAddress'];
+    $street = $_POST['street'];
+    $city = $_POST['city'];
+    $county = $_POST['county'];
+    $postCode = $_POST['postCode'];
+    $status = $_POST['status'];
+    $annualRevenue = $_POST['annualRevenue'];
+
+    $query = "INSERT INTO Contact (company_name, contact_name, title, role, phone_number, extension_number , mobile_number, email_address, street, city, county, postcode, status, annual_revenue)VALUES ( ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? , ? )";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('ssssssssssssss', $companyName, $contactName, $title, $role, $phoneNumber, $extensionNumber, $mobileNumber, $emailAddress, $street, $city, $county, $postCode, $status, $annualRevenue);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0){
+	    echo "<p>contact inserted into database sucessfully.</p>";
+      } else {
+	    echo "<p>An error has occured.<br/>The item was not added.</p>";
+      }
+      $conn->close();
+
+
+}
 }
 ?>
